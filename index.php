70b3b8a6548af020bf5f96f9aaa56293883cb77c
kirillcheck <32092781+kirillcheck@users.noreply.github.com>
2018-06-25 12:53:28 +0300

    Update index.php

--- index.php
+++ index.php
@@ -21,18 +21,18 @@
   });
   
   $app->get('/add/{n1}/{n2}', function ($n1, $n2) use($app) {
-    return '<h2>Сумма: </h2><h3>' . ($n1 - $n2) . '</h3>'; 
-  })->after($hdrs);
-
-  $app->get('/add/{n1}/{n2}', function ($n1, $n2) use($app) {
     return '<h2>Сумма: </h2><h3>' . ($n1 + $n2) . '</h3>'; 
   })->after($hdrs);
-  $app->get('/add/{n1}/{n2}', function ($n1, $n2) use($app) {
-    return '<h2>Сумма: </h2><h3>' . ($n1 / $n2) . '</h3>'; 
+
+  $app->get('/sub/{n1}/{n2}', function ($n1, $n2) use($app) {
+    return '<h2>Вычитание: </h2><h3>' . ($n1 - $n2) . '</h3>'; 
+  })->after($hdrs);
+  $app->get('/div/{n1}/{n2}', function ($n1, $n2) use($app) {
+    return '<h2>Деление: </h2><h3>' . ($n1 / $n2) . '</h3>'; 
   })->after($hdrs);
 
   $app->get('/add/{n1}/{n2}', function ($n1, $n2) use($app) {
-    return '<h2>Сумма: </h2><h3>' . ($n1 * $n2) . '</h3>'; 
+    return '<h2>Умножение: </h2><h3>' . ($n1 * $n2) . '</h3>'; 
   })->after($hdrs);
 
 
